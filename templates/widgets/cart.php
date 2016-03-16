7eacef5ae97a1b71b895acac72da7bfe7f810e32
calvinpy <dani@aplazame.com>
2016-03-16 11:40:06 +0100

    widget data view

--- templates/widgets/cart.php
+++ templates/widgets/cart.php
@@ -8,6 +8,7 @@ if (!defined('ABSPATH')) {
 
 <div
   data-aplazame-simulator=""
+  data-view="cart"
   data-amount="<?php echo Aplazame_Filters::decimals(WC()->cart->total); ?>"
   data-currency="<?php echo get_woocommerce_currency(); ?>">
 </div>
--- templates/widgets/product.php
+++ templates/widgets/product.php
@@ -9,6 +9,7 @@ global $product;
 
 <div
   data-aplazame-simulator=""
+  data-view="product"
   data-amount="<?php echo Aplazame_Filters::decimals($product->get_price()); ?>"
   data-currency="<?php echo get_woocommerce_currency(); ?>"
   data-stock="<?php echo $product->is_in_stock()?'true':'false'; ?>">
