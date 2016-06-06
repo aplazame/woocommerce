ed5a3f6f20e1a202236015c1a8e238bb5495580c
Maks3w <github.maks3w@virtualplanets.net>
2016-06-06 19:29:19 +0200

    CS

--- classes/sdk/Serializers.php
+++ classes/sdk/Serializers.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 class Aplazame_Serializers
@@ -10,92 +10,92 @@ class Aplazame_Serializers
 	/**
 	 * @return string
 	 */
-    public static function woo_version()
-    {
-        if (!function_exists('get_plugins')) {
-            require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-        }
+	public static function woo_version()
+	{
+		if (!function_exists('get_plugins')) {
+			require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+		}
 
-        $woo = get_plugins('/woocommerce');
-        return $woo['woocommerce.php']['Version'];
-    }
+		$woo = get_plugins('/woocommerce');
+		return $woo['woocommerce.php']['Version'];
+	}
 
 	/**
 	 * @return array
 	 */
-    public static function get_meta()
-    {
-        return array(
-            'module' => array(
-                'name' => 'aplazame:woocommerce',
-                'version' => WC_Aplazame::VERSION
-            ),
-            'version' => static::woo_version()
-        );
-    }
+	public static function get_meta()
+	{
+		return array(
+			'module' => array(
+				'name' => 'aplazame:woocommerce',
+				'version' => WC_Aplazame::VERSION
+			),
+			'version' => static::woo_version()
+		);
+	}
 
 	/**
 	 * @param array $items
 	 *
 	 * @return array
 	 */
-    public function get_articles($items)
-    {
-        $articles = array();
+	public function get_articles($items)
+	{
+		$articles = array();
 
-        foreach($items as $item => $values) {
-            $product = new WC_Product($values['product_id']);
+		foreach($items as $item => $values) {
+			$product = new WC_Product($values['product_id']);
 
-            $tax_rate = 100 * ($values['line_tax'] / $values['line_total']);
+			$tax_rate = 100 * ($values['line_tax'] / $values['line_total']);
 
-            $articles[] = array(
-                'id' => $values['product_id'],
-                'sku' => $product->get_sku(),
-                'name' => $values['name'],
-                'description' => $product->get_post_data()->post_content,
-                'url' => $product->get_permalink(),
-                'image_url' => wp_get_attachment_url(
-                    get_post_thumbnail_id($values['product_id'])),
-                'quantity' => (int) $values['qty'],
-                'price' => Aplazame_Filters::decimals($values['line_total']) / (int) $values['qty'],
-                'tax_rate' => Aplazame_Filters::decimals($tax_rate)
-            );
-        }
+			$articles[] = array(
+				'id' => $values['product_id'],
+				'sku' => $product->get_sku(),
+				'name' => $values['name'],
+				'description' => $product->get_post_data()->post_content,
+				'url' => $product->get_permalink(),
+				'image_url' => wp_get_attachment_url(
+					get_post_thumbnail_id($values['product_id'])),
+				'quantity' => (int) $values['qty'],
+				'price' => Aplazame_Filters::decimals($values['line_total']) / (int) $values['qty'],
+				'tax_rate' => Aplazame_Filters::decimals($tax_rate)
+			);
+		}
 
-        return $articles;
-    }
+		return $articles;
+	}
 
 	/**
 	 * @param WP_User $user
 	 *
 	 * @return array
 	 */
-    public function get_user($user)
-    {
+	public function get_user($user)
+	{
 	    $dateJoined = new DateTime($user->user_registered);
 
-        return array(
-            'id' => (string) $user->ID,
-            'type' => 'e',
-            'gender' => 0,
-            'email' => $user->user_email,
-            'first_name' => $user->first_name,
-            'last_name' => $user->last_name,
-            'date_joined' => $dateJoined->format( DATE_ISO8601 ));
-    }
+		return array(
+			'id' => (string) $user->ID,
+			'type' => 'e',
+			'gender' => 0,
+			'email' => $user->user_email,
+			'first_name' => $user->first_name,
+			'last_name' => $user->last_name,
+			'date_joined' => $dateJoined->format( DATE_ISO8601 ));
+	}
 
 	/**
 	 * @param string $billing_email
 	 *
 	 * @return array
 	 */
-    public function get_customer($billing_email)
-    {
-        return array(
-            'type' => 'n',
-            'gender' => 0,
-            'email' => $billing_email);
-    }
+	public function get_customer($billing_email)
+	{
+		return array(
+			'type' => 'n',
+			'gender' => 0,
+			'email' => $billing_email);
+	}
 
 	/**
 	 * @param WC_Order $order
@@ -103,132 +103,132 @@ class Aplazame_Serializers
 	 *
 	 * @return array
 	 */
-    public function get_address($order, $type)
-    {
-        $_field = function($name) use ($order, $type) {
-            $_key = ($type . '_' . $name);
-            return $order->$_key;
-        };
+	public function get_address($order, $type)
+	{
+		$_field = function($name) use ($order, $type) {
+			$_key = ($type . '_' . $name);
+			return $order->$_key;
+		};
 
-        $serializer = array(
-            'first_name' => $_field('first_name'),
-            'last_name' => $_field('last_name'),
-            'phone' => $_field('phone'),
-            'street' => $_field('address_1'),
-            'address_addition' => $_field('address_2'),
-            'city' => $_field('city'),
-            'state' => $_field('state'),
-            'country' => $_field('country'),
-            'postcode' => $_field('postcode'));
+		$serializer = array(
+			'first_name' => $_field('first_name'),
+			'last_name' => $_field('last_name'),
+			'phone' => $_field('phone'),
+			'street' => $_field('address_1'),
+			'address_addition' => $_field('address_2'),
+			'city' => $_field('city'),
+			'state' => $_field('state'),
+			'country' => $_field('country'),
+			'postcode' => $_field('postcode'));
 
-        return $serializer;
-    }
-
-	/**
-	 * @param WC_Order $order
-	 *
-	 * @return array
-	 */
-    public function get_shipping_info($order)
-    {
-        $total = $order->get_total_shipping();
-
-        if ($total) {
-            $tax_rate = 100 * $order->order_shipping_tax / $total;
-        } else {
-            $tax_rate = 0;
-        }
-
-        $serializer = array_merge(
-            $this->get_address($order, 'shipping'), array(
-                'price' => Aplazame_Filters::decimals($total),
-                'tax_rate' => Aplazame_Filters::decimals($tax_rate),
-                'name' => $order->get_shipping_method()
-        ));
-
-        return $serializer;
-    }
+		return $serializer;
+	}
 
 	/**
 	 * @param WC_Order $order
 	 *
 	 * @return array
 	 */
-    public function get_order($order)
-    {
-        $serializer = array(
-            'id' => (string) $order->id,
-            'articles' => $this->get_articles($order->get_items()),
-            'currency' => get_woocommerce_currency(),
-            'total_amount' => Aplazame_Filters::decimals($order->get_total()),
-            'discount' => Aplazame_Filters::decimals(
-                $order->get_total_discount()));
+	public function get_shipping_info($order)
+	{
+		$total = $order->get_total_shipping();
 
-        return $serializer;
-    }
+		if ($total) {
+			$tax_rate = 100 * $order->order_shipping_tax / $total;
+		} else {
+			$tax_rate = 0;
+		}
 
-    /**
-     * @param WC_Order $order
-     * @param string $checkout_url
-     * @param WP_User $user
-     *
-     * @return array
-     */
-    public function get_checkout($order, $checkout_url, $user)
-    {
-        $serializer = array(
-            'toc' => true,
-            'merchant' => array(
-                'confirmation_url' => home_url(
-                    add_query_arg('action', 'confirm')),
-                'cancel_url' => html_entity_decode(
-                    $order->get_cancel_order_url()),
-                'checkout_url' => html_entity_decode(
-                    $order->get_cancel_order_url($checkout_url)),
-                'success_url' => html_entity_decode(
-                    $order->get_checkout_order_received_url())
-            ),
-            'customer' => $user->ID?$this->get_user(
-                $user):$this->get_customer($order->billing_email),
-            'order' => $this->get_order($order),
-            'billing' => $this->get_address($order, 'billing'),
-            'meta' => static::get_meta());
+		$serializer = array_merge(
+			$this->get_address($order, 'shipping'), array(
+				'price' => Aplazame_Filters::decimals($total),
+				'tax_rate' => Aplazame_Filters::decimals($tax_rate),
+				'name' => $order->get_shipping_method()
+			));
 
-        $shipping_method = $order->get_shipping_method();
+			return $serializer;
+	}
 
-        if (!empty($shipping_method)) {
-            $serializer['shipping'] = $this->get_shipping_info($order);
-        }
+	/**
+	 * @param WC_Order $order
+	 *
+	 * @return array
+	 */
+	public function get_order($order)
+	{
+		$serializer = array(
+			'id' => (string) $order->id,
+			'articles' => $this->get_articles($order->get_items()),
+			'currency' => get_woocommerce_currency(),
+			'total_amount' => Aplazame_Filters::decimals($order->get_total()),
+			'discount' => Aplazame_Filters::decimals(
+				$order->get_total_discount()));
 
-        return $serializer;
-    }
+		return $serializer;
+	}
+
+	/**
+	 * @param WC_Order $order
+	 * @param string $checkout_url
+	 * @param WP_User $user
+	 *
+	 * @return array
+	 */
+	public function get_checkout($order, $checkout_url, $user)
+	{
+		$serializer = array(
+			'toc' => true,
+			'merchant' => array(
+				'confirmation_url' => home_url(
+					add_query_arg('action', 'confirm')),
+				'cancel_url' => html_entity_decode(
+					$order->get_cancel_order_url()),
+				'checkout_url' => html_entity_decode(
+					$order->get_cancel_order_url($checkout_url)),
+				'success_url' => html_entity_decode(
+					$order->get_checkout_order_received_url())
+			),
+			'customer' => $user->ID?$this->get_user(
+				$user):$this->get_customer($order->billing_email),
+			'order' => $this->get_order($order),
+			'billing' => $this->get_address($order, 'billing'),
+			'meta' => static::get_meta());
+
+		$shipping_method = $order->get_shipping_method();
+
+		if (!empty($shipping_method)) {
+			$serializer['shipping'] = $this->get_shipping_info($order);
+		}
+
+		return $serializer;
+	}
 
 	/**
 	 * @param array $qs
 	 *
 	 * @return array
 	 */
-    public function get_history($qs)
-    {
-        $orders = array();
+	public function get_history($qs)
+	{
+		$orders = array();
 
-        foreach($qs as $item => $values) {
-            $order = new WC_Order($qs[$item]->ID);
+		foreach($qs as $item => $values) {
+			$order = new WC_Order($qs[$item]->ID);
 	        $orderDate = new DateTime($order->order_date);
 
-            $orders[] = array(
-                'id' => (string) $order->id,
-                'amount' => Aplazame_Filters::decimals($order->get_total()),
-                'due' => '',
-                'status' => $order->get_status(),
-                'type' => Aplazame_Helpers::get_payment_method($order->id),
-                'order_date' => $orderDate->format( DATE_ISO8601 ),
-                'currency' => $order->get_order_currency(),
-                'billing' => $this->get_address($order, 'billing'),
-                'shipping' => $this->get_shipping_info($order)
-            );
-        }
+			$orders[] = array(
+				'id' => (string) $order->id,
+				'amount' => Aplazame_Filters::decimals($order->get_total()),
+				'due' => '',
+				'status' => $order->get_status(),
+				'type' => Aplazame_Helpers::get_payment_method($order->id),
+				'order_date' => $orderDate->format( DATE_ISO8601 ),
+				'currency' => $order->get_order_currency(),
+				'billing' => $this->get_address($order, 'billing'),
+				'shipping' => $this->get_shipping_info($order)
+			);
+		}
 
-        return $orders;
-    }
+		return $orders;
+	}
 }
--- classes/wc-aplazame-gateway.php
+++ classes/wc-aplazame-gateway.php
@@ -1,160 +1,160 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 
 class WC_Aplazame_Gateway extends WC_Payment_Gateway
 {
-    public function __construct()
-    {
-        $this->id = WC_Aplazame::METHOD_ID;
-        $this->method_title = WC_Aplazame::METHOD_TITLE;
-        $this->has_fields = true;
+	public function __construct()
+	{
+		$this->id = WC_Aplazame::METHOD_ID;
+		$this->method_title = WC_Aplazame::METHOD_TITLE;
+		$this->has_fields = true;
 
-        # Settings
-        $this->init_form_fields();
-        $this->init_settings();
+		# Settings
+		$this->init_form_fields();
+		$this->init_settings();
 
-        $this->title = $this->method_title;
-        $this->enabled = $this->settings['enabled'];
-        $this->icon = plugins_url('assets/img/icon.png', dirname(__FILE__));
+		$this->title = $this->method_title;
+		$this->enabled = $this->settings['enabled'];
+		$this->icon = plugins_url('assets/img/icon.png', dirname(__FILE__));
 
-        $this->supports = array(
-            'products',
-            'refunds'
-        );
+		$this->supports = array(
+			'products',
+			'refunds'
+		);
 
-        add_action('woocommerce_update_options_payment_gateways', array(
-            $this, 'process_admin_options'));
+		add_action('woocommerce_update_options_payment_gateways', array(
+			$this, 'process_admin_options'));
 
-        add_action('woocommerce_update_options_payment_gateways_' .
-            $this->id, array($this, 'process_admin_options'));
+		add_action('woocommerce_update_options_payment_gateways_' .
+			$this->id, array($this, 'process_admin_options'));
 
-        add_action('admin_notices', array($this, 'checks'));
-    }
+		add_action('admin_notices', array($this, 'checks'));
+	}
 
-    public function is_available()
-    {
-        if (($this->enabled === 'no') ||
-                (get_woocommerce_currency() !== 'EUR') ||
-                (!$this->settings['public_api_key']) ||
-                (!$this->settings['private_api_key'])) {
-            return false;
-        }
+	public function is_available()
+	{
+		if (($this->enabled === 'no') ||
+				(get_woocommerce_currency() !== 'EUR') ||
+				(!$this->settings['public_api_key']) ||
+				(!$this->settings['private_api_key'])) {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public function payment_fields()
-    {
-        Aplazame_Helpers::render_to_template('gateway/payment-fields.php');
-    }
+	public function payment_fields()
+	{
+		Aplazame_Helpers::render_to_template('gateway/payment-fields.php');
+	}
 
-    public function process_payment($order_id)
-    {
-        $url = get_permalink(Aplazame_Redirect::get_the_ID());
-        WC()->session->redirect_order_id = $order_id;
+	public function process_payment($order_id)
+	{
+		$url = get_permalink(Aplazame_Redirect::get_the_ID());
+		WC()->session->redirect_order_id = $order_id;
 
-        return array(
-            'result' => 'success',
-            'redirect' => add_query_arg(array('order_id' => $order_id), $url)
-        );
-    }
+		return array(
+			'result' => 'success',
+			'redirect' => add_query_arg(array('order_id' => $order_id), $url)
+		);
+	}
 
-    public function process_refund($order_id, $amount=null, $reason='')
-    {
-        if ($amount) {
-            /** @var WC_Aplazame $aplazame */
-            global $aplazame;
+	public function process_refund($order_id, $amount=null, $reason='')
+	{
+		if ($amount) {
+			/** @var WC_Aplazame $aplazame */
+			global $aplazame;
 
-            try {
-                $aplazame->get_client()->refund(
-                    $order_id, Aplazame_Filters::decimals($amount), $reason);
+			try {
+				$aplazame->get_client()->refund(
+					$order_id, Aplazame_Filters::decimals($amount), $reason);
 
-                $aplazame->add_order_note($order_id, sprintf(
-                    __('%s has successfully returned %d %s of the order #%s.', 'aplazame'),
-                    $this->method_title, $amount, get_woocommerce_currency(), $order_id));
+				$aplazame->add_order_note($order_id, sprintf(
+					__('%s has successfully returned %d %s of the order #%s.', 'aplazame'),
+					$this->method_title, $amount, get_woocommerce_currency(), $order_id));
 
-                return true;
-            } catch (Aplazame_Exception $e) {
-                return new WP_Error('aplazame_refund_error', sprintf(
-                    __('%s Error: "%s"', 'aplazame'),
-                    $this->method_title, $e->get_field_error('amount')));
-            }
-        }
+				return true;
+			} catch (Aplazame_Exception $e) {
+				return new WP_Error('aplazame_refund_error', sprintf(
+					__('%s Error: "%s"', 'aplazame'),
+					$this->method_title, $e->get_field_error('amount')));
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function checks()
-    {
-        if (!$this->enabled) {
-            return;
-        }
+	public function checks()
+	{
+		if (!$this->enabled) {
+			return;
+		}
 
-        $_render_to_notice = function($msg) {
-            echo '<div class="error"><p>' . $msg . '</p></div>';
-        };
+		$_render_to_notice = function($msg) {
+			echo '<div class="error"><p>' . $msg . '</p></div>';
+		};
 
-        if (!$this->settings['public_api_key'] || !$this->settings['private_api_key']) {
-            $_render_to_notice(sprintf(__(
-                'Aplazame gateway requires the API keys, please ' .
-                '<a href="%s">contact us</a> and take your keys.', 'aplazame'),
-                'mailto:soporte.woo@aplazame.com?subject=i-need-a-token'));
-        }
-    }
+		if (!$this->settings['public_api_key'] || !$this->settings['private_api_key']) {
+			$_render_to_notice(sprintf(__(
+				'Aplazame gateway requires the API keys, please ' .
+				'<a href="%s">contact us</a> and take your keys.', 'aplazame'),
+				'mailto:soporte.woo@aplazame.com?subject=i-need-a-token'));
+		}
+	}
 
-    public function init_form_fields()
-    {
-        $this->form_fields = array(
-            'enabled' => array(
-                'type' => 'checkbox',
-                'title' => __('Enable/Disable', 'aplazame'),
-                'label' => __('Enable Aplazame module', 'aplazame'),
-                'default' => 'yes'
-            ),
-            'sandbox' => array(
-                'type' => 'checkbox',
-                'title' => 'Sandbox',
-                'description' => __(
-                    'Determines if the module is on Sandbox mode', 'aplazame'),
-                'label' => __('Turn on Sandbox', 'aplazame'),
-            ),
-            'host' => array(
-                'type' => 'text',
-                'title' => 'Host',
-                'description' => __('Aplazame Host', 'aplazame'),
-            ),
-            'api_version' => array(
-                'type' => 'text',
-                'title' => __('API Version', 'aplazame'),
-                'description' => __('Aplazame API Version', 'aplazame'),
-            ),
-            'button' => array(
-                'type' => 'text',
-                'title' => __('Button', 'aplazame'),
-                'description' => __('Aplazame Button CSS Selector', 'aplazame'),
-            ),
-            'public_api_key' => array(
-                'type' => 'text',
-                'title' => __('Public API Key', 'aplazame'),
-                'description' => __('Aplazame Public Key', 'aplazame'),
-            ),
-            'private_api_key' => array(
-                'type' => 'password',
-                'title' => __('Private API Key', 'aplazame'),
-                'description' => __('Aplazame Private Key', 'aplazame'),
-                'custom_attributes' => array(
-                    'autocomplete' => 'new-password',
-                ),
-            ),
-            'enable_analytics' => array(
-                'type' => 'checkbox',
-                'title' => __('Enable/Disable', 'aplazame'),
-                'label' => __('Enable Aplazame Analytics', 'aplazame'),
-            ),
-        );
-    }
+	public function init_form_fields()
+	{
+		$this->form_fields = array(
+			'enabled' => array(
+				'type' => 'checkbox',
+				'title' => __('Enable/Disable', 'aplazame'),
+				'label' => __('Enable Aplazame module', 'aplazame'),
+				'default' => 'yes'
+			),
+			'sandbox' => array(
+				'type' => 'checkbox',
+				'title' => 'Sandbox',
+				'description' => __(
+					'Determines if the module is on Sandbox mode', 'aplazame'),
+				'label' => __('Turn on Sandbox', 'aplazame'),
+			),
+			'host' => array(
+				'type' => 'text',
+				'title' => 'Host',
+				'description' => __('Aplazame Host', 'aplazame'),
+			),
+			'api_version' => array(
+				'type' => 'text',
+				'title' => __('API Version', 'aplazame'),
+				'description' => __('Aplazame API Version', 'aplazame'),
+			),
+			'button' => array(
+				'type' => 'text',
+				'title' => __('Button', 'aplazame'),
+				'description' => __('Aplazame Button CSS Selector', 'aplazame'),
+			),
+			'public_api_key' => array(
+				'type' => 'text',
+				'title' => __('Public API Key', 'aplazame'),
+				'description' => __('Aplazame Public Key', 'aplazame'),
+			),
+			'private_api_key' => array(
+				'type' => 'password',
+				'title' => __('Private API Key', 'aplazame'),
+				'description' => __('Aplazame Private Key', 'aplazame'),
+				'custom_attributes' => array(
+					'autocomplete' => 'new-password',
+				),
+			),
+			'enable_analytics' => array(
+				'type' => 'checkbox',
+				'title' => __('Enable/Disable', 'aplazame'),
+				'label' => __('Enable Aplazame Analytics', 'aplazame'),
+			),
+		);
+	}
 }
