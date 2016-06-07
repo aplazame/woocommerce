a9cc3e8526791445e82ba0f5d26ade16f7f4f725
Maks3w <github.maks3w@virtualplanets.net>
2016-06-07 13:43:54 +0200

    Fix aplazame widget simulator does not update when variable product amount change
    Related:
    https://github.com/aplazame/aplazame-js/commit/b3add5cb857c5557c3583e90f1e406a04f819f6a
    https://github.com/aplazame/aplazame-js/pull/21

--- templates/widgets/product.php
+++ templates/widgets/product.php
@@ -12,11 +12,20 @@ if ( ! $aplazame->enabled ) {
 
 /** @var WC_Product $product */
 global $product;
+
+switch ($product->get_type()) {
+	case 'variable':
+		$price_selector = '#main [itemtype="http://schema.org/Product"] .woocommerce-variation-price .amount';
+		break;
+	default:
+		$price_selector = '#main [itemtype="http://schema.org/Product"] [itemtype="http://schema.org/Offer"] .price .amount';
+}
 ?>
 
 <div
 	data-aplazame-simulator=""
 	data-view="product"
+	data-price='<?php echo $price_selector; ?>'
 	data-amount="<?php echo Aplazame_Filters::decimals( $product->get_price() ); ?>"
 	data-currency="<?php echo get_woocommerce_currency(); ?>"
 	data-stock="<?php echo $product->is_in_stock() ? 'true' : 'false'; ?>">
