034e1fac038566cdc69d8f50b210e44ed8211d9d
Maks3w <github.maks3w@virtualplanets.net>
2016-06-07 14:05:07 +0200

    Don't display widget if variable product does not have a price

--- templates/widgets/product.php
+++ templates/widgets/product.php
@@ -16,9 +16,11 @@ global $product;
 switch ($product->get_type()) {
 	case 'variable':
 		$price_selector = '#main [itemtype="http://schema.org/Product"] .woocommerce-variation-price .amount';
+		$price = '';
 		break;
 	default:
 		$price_selector = '#main [itemtype="http://schema.org/Product"] [itemtype="http://schema.org/Offer"] .price .amount';
+		$price = Aplazame_Filters::decimals( $product->get_price() );
 }
 ?>
 
@@ -26,7 +28,6 @@ switch ($product->get_type()) {
 	data-aplazame-simulator=""
 	data-view="product"
 	data-price='<?php echo $price_selector; ?>'
-	data-amount="<?php echo Aplazame_Filters::decimals( $product->get_price() ); ?>"
 	data-currency="<?php echo get_woocommerce_currency(); ?>"
 	data-stock="<?php echo $product->is_in_stock() ? 'true' : 'false'; ?>">
 </div>
