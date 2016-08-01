924bef8463a3a9c7e4fc27a5535d0d0b0b6e92e6
Maks3w <github.maks3w@virtualplanets.net>
2016-08-01 14:00:33 +0200

    Set data-api-host

--- plugin/templates/layout/header.php
+++ plugin/templates/layout/header.php
@@ -24,6 +24,7 @@ $aplazameJsUri = defined( 'APLAZAME_JS_URI' ) ? APLAZAME_JS_URI : 'https://aplaz
 <script
 	type="text/javascript"
 	src="<?php echo esc_attr( $aplazameJsUri ); ?>"
+	data-api-host="<?php echo $aplazame->apiBaseUri; ?>"
 	data-aplazame="publicKey: <?php echo $aplazame->settings['public_api_key']; ?>"
 	data-sandbox="<?php echo $aplazame->sandbox ? 'true' : 'false'; ?>">
 </script>
