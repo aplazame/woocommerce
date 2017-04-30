12baa665988172039d291cd396a07b2e5c943bb5
Maks3w <github.maks3w@virtualplanets.net>
2017-04-30 22:34:27 +0200

    Fix warnings for WC >= 3

--- plugin/lib/Aplazame/Aplazame/BusinessModel/Address.php
+++ plugin/lib/Aplazame/Aplazame/BusinessModel/Address.php
@@ -10,15 +10,25 @@ class Aplazame_Aplazame_BusinessModel_Address {
 	 */
 	public static function createFromOrder( WC_Order $order, $type ) {
 		$aAddress = new self();
-		$aAddress->first_name = $order->{$type . '_' . 'first_name'};
-		$aAddress->last_name = $order->{$type . '_' . 'last_name'};
-		$aAddress->street = $order->{$type . '_' . 'address_1'};
-		$aAddress->city = $order->{$type . '_' . 'city'};
-		$aAddress->state = $order->{$type . '_' . 'state'};
-		$aAddress->country = $order->{$type . '_' . 'country'};
-		$aAddress->postcode = $order->{$type . '_' . 'postcode'};
-		$aAddress->phone = $order->{$type . '_' . 'phone'};
-		$aAddress->address_addition = $order->{$type . '_' . 'address_2'};
+		foreach ( array(
+			'first_name' => 'first_name',
+			'last_name' => 'last_name',
+			'street' => 'address_1',
+			'city' => 'city',
+			'state' => 'state',
+			'country' => 'country',
+			'postcode' => 'postcode',
+			'phone' => 'phone',
+			'address_addition' => 'address_2',
+		) as $key => $field ) {
+			$field = $type . '_' . $field;
+
+			if (method_exists($order, 'get_' . $field)) {
+				$aAddress->$key = $order->{'get_' . $field}();
+			} else {
+				$aAddress->$key = $order->$field;
+			}
+		}
 
 		return $aAddress;
 	}
--- plugin/lib/Aplazame/Aplazame/BusinessModel/Customer.php
+++ plugin/lib/Aplazame/Aplazame/BusinessModel/Customer.php
@@ -46,7 +46,11 @@ class Aplazame_Aplazame_BusinessModel_Customer {
 
 	public static function createGuessCustomerFromOrder( WC_Order $order ) {
 		$aCustomer = new self();
-		$aCustomer->email = $order->billing_email;
+		if (method_exists($order, 'get_id')) {
+			$aCustomer->email = $order->get_billing_email();
+		} else {
+			$aCustomer->email = $order->billing_email;
+		}
 		$aCustomer->type = 'g';
 		$aCustomer->gender = 0;
 
