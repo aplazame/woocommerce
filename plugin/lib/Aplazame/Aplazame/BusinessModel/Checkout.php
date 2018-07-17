9f19299e5d787a1ef21fdf6c115a16187ad6e11a
Maks3w <github.maks3w@virtualplanets.net>
2018-07-17 17:16:18 +0200

    Add history_url

--- plugin/lib/Aplazame/Aplazame/BusinessModel/Checkout.php
+++ plugin/lib/Aplazame/Aplazame/BusinessModel/Checkout.php
@@ -6,17 +6,15 @@
 class Aplazame_Aplazame_BusinessModel_Checkout {
 
 	public static function createFromOrder( WC_Order $order, $checkout_url ) {
+		$apiRouter = WC()->api_request_url( 'aplazame' );
+
 		$merchant = new stdClass();
 		$merchant->cancel_url = html_entity_decode( $order->get_cancel_order_url() );
 		$merchant->checkout_url = html_entity_decode( $checkout_url );
 		$merchant->success_url = html_entity_decode( $order->get_checkout_order_received_url() );
 		$merchant->pending_url = $merchant->success_url;
-		$merchant->notification_url = add_query_arg(
-			array(
-				'path' => '/confirm/',
-			),
-			WC()->api_request_url( 'aplazame' )
-		);
+		$merchant->notification_url = add_query_arg( array( 'path' => '/confirm/' ), $apiRouter );
+		$merchant->history_url = add_query_arg( array( 'path' => '/order/{order_id}/history/' ), $apiRouter );
 
 		$checkout = new self();
 		$checkout->toc = true;
