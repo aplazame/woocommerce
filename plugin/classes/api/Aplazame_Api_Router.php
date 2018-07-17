5076586273111a2678376cd203ab219258cc4e21
Maks3w <github.maks3w@virtualplanets.net>
2018-07-17 16:50:48 +0200

    Remove authentication with Authorization header

--- plugin/classes/api/Aplazame_Api_Router.php
+++ plugin/classes/api/Aplazame_Api_Router.php
@@ -137,45 +137,6 @@ class Aplazame_Api_Router {
 			return $token;
 		}
 
-		if ( function_exists( 'getallheaders' ) ) {
-			$headers = getallheaders();
-		} else {
-			$headers = $this->getallheaders();
-		}
-		$headers = array_change_key_case( $headers, CASE_LOWER );
-
-		if ( isset( $headers['authorization'] ) ) {
-			return trim( str_replace( 'Bearer', '', $headers['authorization'] ) );
-		}
-
 		return false;
-	}
-
-	private function getallheaders() {
-		$headers = array();
-		$copy_server = array(
-			'CONTENT_TYPE'   => 'content-type',
-			'CONTENT_LENGTH' => 'content-length',
-			'CONTENT_MD5'    => 'content-md5',
-		);
-
-		foreach ( $_SERVER as $name => $value ) {
-			if ( substr( $name, 0, 5 ) === 'HTTP_' ) {
-				$name = substr( $name, 5 );
-				if ( ! isset( $copy_server[ $name ] ) || ! isset( $_SERVER[ $name ] ) ) {
-					$headers[ str_replace( ' ', '-', strtolower( str_replace( '_', ' ', $name ) ) ) ] = $value;
-				}
-			} elseif ( isset( $copy_server[ $name ] ) ) {
-				$headers[ $copy_server[ $name ] ] = $value;
-			}
-		}
-
-		if ( ! isset( $headers['authorization'] ) ) {
-			if ( isset( $_SERVER['REDIRECT_HTTP_AUTHORIZATION'] ) ) {
-				$headers['authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
-			}
-		}
-
-		return $headers;
 	}
 }
