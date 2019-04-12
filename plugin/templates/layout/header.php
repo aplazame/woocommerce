82dbbf9f5b41877fac0a4b02970943d10733cc41
Alejandro Hernández <alejandro.herord@gmail.com>
2019-04-12 13:36:28 +0200

    Use http_build_query for aplazame.js params

--- plugin/templates/layout/header.php
+++ plugin/templates/layout/header.php
@@ -14,8 +14,19 @@ if ( ! $aplazame->enabled ) {
 	return;
 }
 
-$aplazameJsUri = defined( 'APLAZAME_JS_URI' ) ? APLAZAME_JS_URI : 'https://cdn.aplazame.com/aplazame.js?public_key='
-	. $aplazame->settings['public_api_key'] . '&sandbox=' . ( $aplazame->sandbox ? 'true' : 'false' );
+$aplazameJsUri = defined( 'APLAZAME_JS_URI' ) ? APLAZAME_JS_URI : 'https://cdn.aplazame.com/aplazame.js';
+
+$aplazameJsParams = http_build_query(
+	array(
+		'public_key' => $aplazame->settings['public_api_key'],
+		'sandbox'    => $aplazame->sandbox ? 'true' : 'false',
+	)
+);
 ?>
 
-<script type="text/javascript" src="<?php echo esc_attr( $aplazameJsUri ); ?>" data-api-host="<?php echo $aplazame->apiBaseUri; ?>" async defer></script>
+<script
+	type="text/javascript"
+	src="<?php echo esc_attr( $aplazameJsUri ); ?>?<?php echo esc_attr( $aplazameJsParams ); ?>"
+	data-api-host="<?php echo $aplazame->apiBaseUri; ?>"
+	async defer
+></script>
