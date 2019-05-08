3102e8b82a89b00b9e9695fe6736cc66c20de422
Alejandro Hernández <alejandro.herord@gmail.com>
2019-05-08 12:23:34 +0200

    Add 'host' as query param

--- plugin/templates/layout/header.php
+++ plugin/templates/layout/header.php
@@ -16,10 +16,13 @@ if ( ! $aplazame->enabled ) {
 
 $aplazameJsUri = defined( 'APLAZAME_JS_URI' ) ? APLAZAME_JS_URI : 'https://cdn.aplazame.com/aplazame.js';
 
-$aplazameJsParams = http_build_query(
-	array(
-		'public_key' => $aplazame->settings['public_api_key'],
-		'sandbox'    => $aplazame->sandbox ? 'true' : 'false',
+$aplazameJsParams = urldecode(
+	http_build_query(
+		array(
+			'public_key' => $aplazame->settings['public_api_key'],
+			'sandbox'    => $aplazame->sandbox ? 'true' : 'false',
+			'host'       => $aplazame->apiBaseUri,
+		)
 	)
 );
 ?>
@@ -27,6 +30,5 @@ $aplazameJsParams = http_build_query(
 <script
 	type="text/javascript"
 	src="<?php echo esc_attr( $aplazameJsUri ); ?>?<?php echo esc_attr( $aplazameJsParams ); ?>"
-	data-api-host="<?php echo $aplazame->apiBaseUri; ?>"
 	async defer
 ></script>
